fcd8e3f9c9daff90c0cb2dda332ddb802883c4c6
Jonas Bernardino <jonasl.bernardino@gmail.com>
2026-09-02 20:52:43 -0300

    first commit

---
+++ src/main/java/br/edu/uniesp/assistencia/internal/equipamento/entity/EquipamentoEntity.java
@@ -0,0 +1,8 @@
+package br.edu.uniesp.assistencia.internal.equipamento.entity;
+
+import br.edu.uniesp.assistencia.internal.cliente.entity.ClienteEntity;
+
+public class EquipamentoEntity {
+
+    private ClienteEntity cliente;
+}
